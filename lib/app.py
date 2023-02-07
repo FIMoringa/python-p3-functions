de07c76af4531afff5963a3807f740333680251e
Titus Muthomi <muthomititus+github@gmail.com>
2023-02-07 11:49:16 +0300

    Add functions to pass lab tests

---
+++ lib/app.py
@@ -0,0 +1,23 @@
+
+
+def  greetProgrammer():
+  print("Hello, programmer!")
+
+
+def greet(name):
+  print("Hello, Naureen!")
+
+
+def greetWithDefault(name):
+  print("Hello, Sunny!")
+
+
+def add(a, b):
+  return a + b
+
+
+def halve(number):
+  return number / 2
+
+
+greetProgrammer()
